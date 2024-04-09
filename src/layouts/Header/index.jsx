a277d6782b7926b4fe5a811c92ec7b068f82815a
Timw <timw051201@gmail.com>
2024-04-09 13:55:43 +0700

    Update newLayout

---
+++ src/layouts/Header/index.jsx
@@ -0,0 +1,5 @@
+import { HeaderContainer } from "./styled";
+
+export default function Header() {
+  return <HeaderContainer>Header</HeaderContainer>;
+}
--- src/views/HomePage/index.jsx
+++ src/views/HomePage/index.jsx
@@ -1,3 +1,8 @@
 export default function HomePages() {
-  return <div>HomePages</div>;
+  return <div className="h-100vh flex-col flow-y-auto">
+    <div className="header">
+        
+    </div>
+  
+  </div>;
 }
